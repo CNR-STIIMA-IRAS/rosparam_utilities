278fedc2b83f5ed185f0f367c93a30d8648a8b9b
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2022-03-08 11:37:15 +0100

    isolation and code coverage fix

--- .coverage.sh
+++ .coverage.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-source "${ICI_SRC_PATH}/workspace.sh"
-source "${ICI_SRC_PATH}/util.sh"
-
-echo "Generating coverage for 'rosparam_utilities'"
+declare -a StringArray=("rosparam_utilities")
 
 ws=~/target_ws
-extend="/opt/ros/$ROS_DISTRO"
-ici_exec_in_workspace "$extend" "$ws" catkin build rosparam_utilities -v --no-deps --catkin-make-args coverage_report
 
-echo "Uploading coverage results to codecov.io"
+cp /home/runner/work/cnr_control_toolbox/cnr_control_toolbox/codecov.yml "$ws"/
+
+cd "$ws"
+
+source ./devel/setup.bash
+
+for val in ${StringArray[@]}; do
+
+    echo "Generating coverage for '$val'"
+
+    catkin build "$val" --no-deps --catkin-make-args run_tests 
+    catkin build "$val" --no-deps --catkin-make-args coverage_report --cmake-args -DENABLE_COVERAGE_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DCATKIN_ENABLE_TESTING=ON
+done
 
 # Remove duplicated information
-rm "$ws/build/rosparam_utilities/coverage_report.info.cleaned"
-rm "$ws/build/rosparam_utilities/coverage_report.info.removed"
-
-# Actually upload coverage information
-bash <(curl -s https://codecov.io/bash) -s "$ws/build/rosparam_utilities/"
+find "$ws" -name \*.info.cleaned -exec rm {} \;
+find "$ws" -name \*.info.removed -exec rm {} \;
